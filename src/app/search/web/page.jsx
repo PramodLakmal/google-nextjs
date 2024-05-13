8595dbb4cb1a7e152c8c7ee28d3ec959c9201d90
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-05-13 18:52:10 +0530

    create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 import React from "react";
 
@@ -27,6 +28,6 @@ export default async function WebSearchPage({ searchParams }) {
   }
 
   return (
-    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+    <div>{results &&  <WebSearchResults results={data}/>}</div>
   );
 }
